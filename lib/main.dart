d581382454b6c50084b65120a6f7e23c49f9b5fe
abdullahmansss <abdullah.mansour97@gmail.com>
2021-03-30 23:05:25 +0200

    messenger screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_flutter/login_screen.dart';
+import 'package:udemy_flutter/messenger_screen.dart';
 
 void main()
 {
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget
   {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: MessengerScreen(),
     );
   }
 }
