00929de823c64f9b125f4c4effa6347ca6414a9c
abdullahmansss <abdullah.mansour97@gmail.com>
2021-04-02 02:45:26 +0200

    form validation

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatelessWidget
 {
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context)
@@ -22,100 +23,101 @@ class LoginScreen extends StatelessWidget
         padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:
+                [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  onFieldSubmitted: (String value) {
-                    print(value);
-                  },
-                  onChanged: (String value) {
-                    print(value);
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Email Address',
-                    prefixIcon: Icon(
-                      Icons.email,
-                    ),
-                    border: OutlineInputBorder(),
+                  SizedBox(
+                    height: 40.0,
                   ),
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  onFieldSubmitted: (String value) {
-                    print(value);
-                  },
-                  onChanged: (String value) {
-                    print(value);
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    prefixIcon: Icon(
-                      Icons.lock,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.remove_red_eye,
-                    ),
-                    border: OutlineInputBorder(),
+                  defaultFormField(
+                    controller: emailController,
+                    label: 'Email',
+                    prefix: Icons.email,
+                    type: TextInputType.emailAddress,
+                    validate: (String value)
+                    {
+                      if(value.isEmpty)
+                      {
+                        return 'email must not be empty';
+                      }
+
+                      return null;
+                    },
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                defaultButton(
-                  text: 'login',
-                  function: ()
-                  {
-                    print(emailController.text);
-                    print(passwordController.text);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                defaultButton(
-                  text: 'ReGIster',
-                  function: ()
-                  {
-                    print(emailController.text);
-                    print(passwordController.text);
-                  },
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Register Now',
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  defaultFormField(
+                    controller: passwordController,
+                    label: 'Password',
+                    prefix: Icons.lock,
+                    suffix: Icons.remove_red_eye,
+                    isPassword: true,
+                    type: TextInputType.visiblePassword,
+                    validate: (String value)
+                    {
+                      if(value.isEmpty)
+                      {
+                        return 'password is too short';
+                      }
+
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  defaultButton(
+                    text: 'login',
+                    function: ()
+                    {
+                      if(formKey.currentState.validate())
+                      {
+                        print(emailController.text);
+                        print(passwordController.text);
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  defaultButton(
+                    text: 'ReGIster',
+                    function: ()
+                    {
+                      print(emailController.text);
+                      print(passwordController.text);
+                    },
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Don\'t have an account?',
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Register Now',
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -27,3 +27,32 @@ Widget defaultButton({
         color: background,
       ),
     );
+
+Widget defaultFormField({
+  @required TextEditingController controller,
+  @required TextInputType type,
+  Function onSubmit,
+  Function onChange,
+  bool isPassword = false,
+  @required Function validate,
+  @required String label,
+  @required IconData prefix,
+  IconData suffix,
+}) => TextFormField(
+  controller: controller,
+  keyboardType: type,
+  obscureText: isPassword,
+  onFieldSubmitted: onSubmit,
+  onChanged: onChange,
+  validator: validate,
+  decoration: InputDecoration(
+    labelText: label,
+    prefixIcon: Icon(
+      prefix,
+    ),
+    suffixIcon: suffix != null ? Icon(
+      suffix,
+    ) : null,
+    border: OutlineInputBorder(),
+  ),
+);
